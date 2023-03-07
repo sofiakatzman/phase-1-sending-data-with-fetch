bf378030b85c302c957c69e9fa8aab2db3c5e28f
Sofia Katzman <katzmansof@gmail.com>
2023-03-06 20:18:32 -0500

    finsihed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData (name, email){
+    //In submitData, write a valid POST request to http://localhost:3000/users using fetch()
+    return fetch ('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        }) 
+      })
+      .then(res => res.json())
+      .then(body => addNewData(body.id))
+      .catch(error =>
+          document.body.innerHTML = error.message
+      )
+      }
+
+      function addNewData(newID){
+          document.getElementsByTagName("body")[0].innerHTML = newID
+      }
+
